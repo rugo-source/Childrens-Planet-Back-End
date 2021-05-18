1b3ef487bc11c1e180c59d51413795735e9e5ac2
rugo-source <rugo@live.com.mx>
2021-05-18 18:29:15 -0500

    fixed issue with controller games

--- app/controllers/controllers.Games.js
+++ app/controllers/controllers.Games.js
@@ -5,9 +5,9 @@ const {Op}=require("sequelize");
 exports.findAll = async (req, res) => {
     const amount = req.query.amount;
    
-    Game.findAll({ where: { amount: {[Op.or]: {[Op.lt]: 1}}}})
-      .then((game) => {
-        res.send(game);
+    Game.findAll({ where: { amount: {[Op.or]: {[Op.gt]: 0}}}})
+      .then((games) => {
+        res.send(games);
       })
       .catch((err) => {
         res.status(500).send({ message: err.message });
